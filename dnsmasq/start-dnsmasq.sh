9235c24f95aae0560b02d516f52107a12d868058
Nils Schimmelmann <nschimme@gmail.com>
2017-11-29 01:44:33 +0000

    Support for alternative DNS

--- dnsmasq/start-dnsmasq.sh
+++ dnsmasq/start-dnsmasq.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 [[ -n $HOST_IP ]] || (echo "\$HOST_IP not set. Cannot proceed :(" && exit 1)
+[[ -n $UPSTREAM_DNS_IP ]] || (echo "\$UPSTREAM_DNS_IP not set. Cannot proceed :(" && exit 1)
 
 [[ -f /etc/dnsmasq.d/custom-zones.conf ]] || (cat /dnsmasq-template.conf | sed "s/IP_HERE/$HOST_IP/" > /etc/dnsmasq.d/custom-zones.conf)
+[[ -f /etc/resolv.dnsmasq.conf ]] || (cat /resolv-template.conf | sed "s/DNS_HERE/$UPSTREAM_DNS_IP/" > /etc/resolv.dnsmasq.conf)
 
 exec dnsmasq --no-daemon
 
--- dnsmasq/start.sh
+++ dnsmasq/start.sh
@@ -6,8 +6,14 @@ get_ip() {
 
 }
 
+get_dns_ip() {
+  cat /etc/resolv.conf |grep -i '^nameserver'|head -n1|cut -d ' ' -f2
+}
+
 [[ -z $HOST_IP ]] && export HOST_IP=$(get_ip) &&  echo "\$HOST_IP not set - using $HOST_IP"
 [[ -n $HOST_IP ]] || (echo "\$HOST_IP not set and couldn't detect it. Bailing hard." && exit 1)
+[[ -z $UPSTREAM_DNS_IP ]] && export UPSTREAM_DNS_IP=$(get_dns_ip) && echo "\$UPSTREAM_DNS_IP not set - using $UPSTREAM_DNS_IP"
+[[ -n $UPSTREAM_DNS_IP ]] || (echo "\$UPSTREAM_DNS_IP not set and couldn't detect it. Bailing hard." && exit 1)
 
 docker rm -f cache_dnsmasq
-docker run -it -d -e "HOST_IP=$HOST_IP" -p 53:53 -p 53:53/udp --name cache_dnsmasq dnsmasq
+docker run -it -d --restart=always -e "HOST_IP=$HOST_IP" -e "UPSTREAM_DNS_IP=$UPSTREAM_DNS_IP" -p 53:53 -p 53:53/udp --name cache_dnsmasq dnsmasq
